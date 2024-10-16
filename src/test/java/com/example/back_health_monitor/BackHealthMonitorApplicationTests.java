682e769e27b9c169710f58bd89039c4d55478ee4
Gabriel Furquim da Silva <88550409+Gabriel7fs@users.noreply.github.com>
2024-10-15 23:48:59 -0300

    new repository

---
+++ src/test/java/com/example/back_health_monitor/BackHealthMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_health_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackHealthMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
